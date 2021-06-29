5b0665b784a9d3d028ba5e4ebecf2e094f90b7af
Agustin Rangel <agustinran.ib@gmail.com>
2021-06-29 13:38:49 -0500

    Stuff and settings to deploy

--- src/components/Bienvenida.js
+++ src/components/Bienvenida.js
@@ -4,21 +4,25 @@ import video_bienvenida from "../assets/videos/BuenasPracticas.mp4";
 const Bienvenida = () => {
 	return (
 		<>
-			<h4 style={{ textAlign: "left" }}>BIENVENIDA</h4> <br/>
+			<h4 style={{ textAlign: "left" }}>BIENVENIDA</h4> <br />
 			<div className="d-flex flex-column align-items-center">
 				<video controls className="video_bienvenida">
 					<source src={video_bienvenida} type="video/mp4" />
 					Your browser does not support HTML5 video.
-				</video> <br />
+				</video>{" "}
+				<br />
 				<p>Al concluir el video, contesta la encuesta y sigue revisando el curso</p>
-				<iframe
+				<p>
+					Para la encuesta de Bienvenida da click{" "}
+					<a href="https://forms.gle/cT6ZK3QiEFbqzrfK6" target="blank">aquí</a>
+				</p>
+				{/* <iframe
 					title="Encuesta de Bienvenida"
 					src="https://h5p6.tecnologia-educativa.com.mx/wp-admin/admin-ajax.php?action=h5p_embed&id=4"
 					frameborder="0"
 					allowfullscreen="allowfullscreen"
 					className="encuesta_bienvenida"
-				></iframe>
-
+				></iframe> */}
 				<Contenidos />
 			</div>
 		</>
--- src/components/Contenido/AntesComenzar.js
+++ src/components/Contenido/AntesComenzar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import AntesDeComenzar from "../../assets/plecas/AntesComenzar3.jpg";
 
 const AntesComenzar = () => {
@@ -7,6 +7,7 @@ const AntesComenzar = () => {
 			<div className="pleca">
 				<img src={AntesDeComenzar} alt="Antes de Comenzar" className="pleca_de_contenido" />
 			</div>
+			<div>contenido de la unidad</div>
 			<iframe
 				src="https://h5p6.tecnologia-educativa.com.mx/wp-admin/admin-ajax.php?action=h5p_embed&id=6"
 				width="776"
@@ -15,12 +16,15 @@ const AntesComenzar = () => {
 				allowfullscreen="allowfullscreen"
 				title="Mapa Buenas prácticas de software"
 			></iframe>
+			<div>
+				<a href="">Metodología de estudio va a estar basasda en estudio de casos</a>
+			</div>
 			<script
 				src="https://h5p6.tecnologia-educativa.com.mx/wp-content/plugins/h5p/h5p-php-library/js/h5p-resizer.js"
 				charset="UTF-8"
 			></script>
 		</div>
 	);
-}
+};
 
-export default AntesComenzar
+export default AntesComenzar;
