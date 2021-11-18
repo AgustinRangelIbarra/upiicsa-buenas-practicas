88ee839d11a449d43734104ca185a13a31c5b611
Agustin Rangel <agustinran.ib@gmail.com>
2021-11-18 03:49:06 -0600

    unidad 1 terminada

--- src/components/Unidades/Unidad1/Sesion.js
+++ src/components/Unidades/Unidad1/Sesion.js
@@ -3,6 +3,7 @@ import { Accordion } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import PlecaIntro from "../../../assets/plecas/5P_15-05/Pleca_BPS_Sesiones.jpg";
 import { sesiones } from "./sesiones.utils";
+import "../../../scss/_sesion.scss";
 
 const Sesion = () => {
 	console.log(sesiones);
@@ -12,59 +13,133 @@ const Sesion = () => {
 				<img src={PlecaIntro} alt="Unidad 1" className="pleca_de_contenido" />
 			</div>
 
-			{sesiones.map((sesion) => (
-				<div key={sesion.id}>
-					<h4 className="blue_text text-center">{sesion.titulo_sesiones}</h4>
-					<h5 className="blue_text text-center">{sesion.tema}</h5>
+			<div className="sesions_main mt-4">
+				{sesiones.map((sesion) => (
+					<div key={sesion.id} className="sesion_container m-2">
+						<h4 className="blue_text text-center">{sesion.titulo_sesiones}</h4>
+						<h5 className="blue_text text-center">{sesion.tema}</h5>
 
-					<Accordion defaultActiveKey="0">
-						<Accordion.Item eventKey="0">
-							<Accordion.Header>Actividad de Aprendizaje: Extra clase</Accordion.Header>
-							{sesion.extra_clase && (
-								<Accordion.Body>
-									{sesion.extra_clase.actividad_aprendizaje1.sesion_linea.temas.map(
-										(tema, index) => (
+						<Accordion defaultActiveKey="0">
+							<Accordion.Item eventKey="0">
+								<Accordion.Header>Actividad de Aprendizaje: Extra clase</Accordion.Header>
+								{sesion.extra_clase && (
+									<Accordion.Body>
+										<h6>{sesion.extra_clase.actividad_aprendizaje1.titulo}</h6>
+										<p>Antes de la sesión en línea</p>
+										<hr />
+										{sesion.extra_clase.actividad_aprendizaje1.sesion_linea.temas.map(
+											(tema, index_) => (
+												<>
+													<div>
+														{index_ + 1}. {tema.titulo}
+													</div>
+													{tema.subtemas && (
+														<>
+															{tema.subtemas.map((subtema, index) => (
+																<div>
+																	<div style={{ margin: "0 0 0 20px" }}>
+																		{index_ + 1}.1.{index + 1} {subtema.titulo}
+																	</div>
+																</div>
+															))}
+														</>
+													)}
+												</>
+											)
+										)}
+										{sesion.id === "7" && (
 											<>
-												<h5>
-													{index + 1}. {tema.titulo}
-												</h5>
-												{tema.subtemas && (
-													<h6>
-														{tema.subtemas.map((subtema, index) => (
-															<div>
-																<h6 className="ml-4">
-																	{index}.1.{index + 1} {subtema.titulo}
-																</h6>
-															</div>
-														))}
-													</h6>
-												)}
+												<hr />
+												<p>Contesta las preguntas de reflexión.</p>
+												<p>
+													Da click{" "}
+													<a
+														target="_blank"
+														href="https://docs.google.com/forms/d/e/1FAIpQLSfJWIv0Rl-Td3Ikw7FF0Uv8ixvUBQYxRsJm5J6VXLfjzg31hg/viewform?usp=sf_link"
+													>
+														aquí
+													</a>
+												</p>
+												<p>Toma en cuenta los criterios de evaluación. </p>
+
+												<div className="container">
+													<p className="text-center">Tabla 1. Actividades de aprendizaje</p>
+
+													<table class="table">
+														<thead>
+															<tr>
+																<th scope="col">Consideración</th>
+																<th scope="col">Muy bien</th>
+																<th scope="col">Bien</th>
+																<th scope="col">Mal</th>
+															</tr>
+														</thead>
+														<tbody>
+															<tr>
+																<th scope="row">Contenido</th>
+																<td>Contesta todas las preguntas. </td>
+																<td>Contesta la mayoría de la preguntas. </td>
+																<td>Contesta algunas de la preguntas.</td>
+															</tr>
+															<tr>
+																<th scope="row">Originalidad</th>
+																<td>Expresa con su reflexión y con claridad y fundamentos.</td>
+																<td>Demuestra poca reflexión y claridad en las ideas. </td>
+																<td>Entrega una copia textual del tema.</td>
+															</tr>
+															<tr>
+																<th scope="row">Puntualidad </th>
+																<td >Entrega en la fecha establecida.</td>
+																<td>Entrega en la fecha pero no en la hora. </td>
+																<td>Entrega a destiempo.</td>
+															</tr>
+														</tbody>
+													</table>
+												</div>
 											</>
-										)
-									)}
-								</Accordion.Body>
+										)}
+									</Accordion.Body>
+								)}
+							</Accordion.Item>
+							{sesion.sesion_aula.sesion_videoconferencia && (
+								<Accordion.Item eventKey="1">
+									<Accordion.Header>Sesión en el aula</Accordion.Header>
+									<Accordion.Body>
+										{sesion.sesion_aula.sesion_videoconferencia.temas.map((tema, index) => (
+											<>
+												<h6>{tema.titulo}</h6>
+												<hr />
+												{tema.parrafos?.length > 0 && <p>asdf</p>}
+											</>
+										))}
+									</Accordion.Body>
+								</Accordion.Item>
 							)}
-						</Accordion.Item>
-						{sesion.extra_clase.actividad_aprendizaje1 && (
-							<Accordion.Item eventKey="1">
-								<Accordion.Header>Sesión en el aula</Accordion.Header>
-								<Accordion.Body></Accordion.Body>
-							</Accordion.Item>
-						)}
-						{sesion.actividad_colaborativo.actividad_aprendizaje2 && (
-							<Accordion.Item eventKey="2">
-								<Accordion.Header>Actividad de aprendizaje colaborativo</Accordion.Header>
-								<Accordion.Body>
-									<h6>{sesion.actividad_colaborativo.actividad_aprendizaje2.titulo}</h6>
-									{sesion.actividad_colaborativo.actividad_aprendizaje2.parrafos.map((parrafo) => (
-										<p>{parrafo}</p>
-									))}
-								</Accordion.Body>
-							</Accordion.Item>
-						)}
-					</Accordion>
-				</div>
-			))}
+							{sesion.actividad_colaborativo.actividad_aprendizaje2 && (
+								<Accordion.Item eventKey="2">
+									<Accordion.Header>Actividad de aprendizaje colaborativo</Accordion.Header>
+									<Accordion.Body>
+										<h6>{sesion.actividad_colaborativo.actividad_aprendizaje2.titulo}</h6>
+										{sesion.actividad_colaborativo.actividad_aprendizaje2.parrafos.map(
+											(parrafo) => (
+												<p>{parrafo}</p>
+											)
+										)}
+										<hr />
+										{sesion.actividad_colaborativo.actividad_aprendizaje2.instrucciones.map(
+											(instruccion, index) => (
+												<p>
+													{index + 1}.- {instruccion}
+												</p>
+											)
+										)}
+									</Accordion.Body>
+								</Accordion.Item>
+							)}
+						</Accordion>
+					</div>
+				))}
+			</div>
 
 			<div className="flex-arrows">
 				<Link to="/unidad1/mapa_actividades">
--- src/components/Unidades/Unidad1/sesiones.utils.js
+++ src/components/Unidades/Unidad1/sesiones.utils.js
@@ -54,6 +54,135 @@ export const sesiones = [
 					{
 						titulo:
 							"Plantea tus dudas  del tema estudiado previamente. Contesta las preguntas dirigidas",
+						parrafos: [],
+					},
+				],
+			},
+		},
+		actividad_colaborativo: {
+			actividad_aprendizaje2: {
+				titulo: "Actividad de aprendizaje 2: Análisis de las buenas prácticas",
+				parrafos: [
+					"En el aula presencial o mediante la videoconferencia, ésta última deberá ser grabada.",
+				],
+				instrucciones: [
+					"En equipo comenten y consensuen el contenido de las infografías que elaboraron previamente de manera individual.",
+					"Elaboren una sola infografía y publíquenla en la sección de tareas.",
+					"Recuerda consultar los criterios de evaluación de los organizadores gráficos.",
+					"Asiste a las sesiones, plantea tus dudas y contesta las preguntas dirigidas. ",
+				],
+			},
+		},
+	},
+	{
+		id: "5",
+		titulo_sesiones: "Sesión 5 y 6",
+		tema: "Marcos de trabajo",
+		extra_clase: {
+			actividad_aprendizaje1: {
+				titulo: "Actividad de aprendizaje 3: Marcos de trabajo",
+				sesion_linea: {
+					temas: [
+						{
+							titulo: 'Consulta el recurso "Marcos de trabajo" ',
+						},
+						{
+							titulo: "Realiza la indagación en Internet, sobre marcos de trabajo de software.",
+						},
+						{
+							titulo: "Elabora un organizador gráfico de los temas.",
+							subtemas: [
+								{
+									id: "1.1.1",
+									titulo: "Especificación de marcos de trabajo",
+								},
+								{
+									id: "1.1.2",
+									titulo: "Objetivo de implementar marcos de trabajo",
+								},
+								{
+									id: "1.1.3",
+									titulo: "Características de los marcos de trabajo.",
+								},
+								{
+									id: "1.1.4",
+									titulo: "Beneficios de utilizar marcos de trabajo.",
+								},
+							],
+						},
+						{
+							titulo:
+								"Recuerda consultar los criterios de evaluación de los organizadores gráficos.",
+						},
+						{
+							titulo:
+								"Publica el archivo con tu infografía y organizador gráfico en la sección de tareas.",
+						},
+					],
+				},
+			},
+		},
+		sesion_aula: {
+			sesion_videoconferencia: {
+				temas: [
+					{
+						titulo:
+							"Plantea tus dudas. Contesta las preguntas dirigidas del profesor. Participa con una reflexión sobre la importancia del tema.",
+						parrafos: [],
+					},
+				],
+			},
+		},
+		actividad_colaborativo: {
+			actividad_aprendizaje2: {
+				titulo: "Actividad de aprenizaje 4: Análisis de marcos de trabajo",
+				parrafos: [
+					"En el aula presencial o mediante la videoconferencia, ésta última deberá ser grabada.",
+				],
+				instrucciones: [
+					"En equipo comenten el contenido de las infografías que elaboraron previamente de manera individual. Llegando a obtener los siguientes conceptos.",
+					"Elaboren una sola infografía y publíquenla en la sección de tareas.",
+					"Asiste a las sesiones, plantea tus dudas.",
+					"Realiza la exposición del tema.",
+				],
+			},
+		},
+	},
+	{
+		id: "7",
+		titulo_sesiones: "Sesión 7 y 8",
+		tema: "Contextualización de la calidad del software.",
+		extra_clase: {
+			actividad_aprendizaje1: {
+				titulo: "Actividad de aprendizaje 5: Contextualización de la calidad del software.",
+				sesion_linea: {
+					temas: [
+						{
+							titulo: 'Consulta el recurso "Calidad de software" ',
+						},
+						{
+							titulo: "Elabora una nube de palabras y publícala en la sección de tareas.",
+						},
+						{
+							titulo: "Asiste a las sesiones y plantea tus dudas.",
+						},
+						{
+							titulo: "Contesta las preguntas de reflexión.",
+						},
+						{
+							titulo: "Considera los criterios de evaluación de las respuestas de reflexión.",
+						},
+					],
+				},
+			},
+		},
+		sesion_aula: {
+			sesion_videoconferencia: {
+				temas: [
+					{
+						titulo:
+							"Plantea tus dudas  del tema estudiado previamente. Contesta las preguntas dirigidas",
+						parrafos: [],
 					},
 				],
 			},
@@ -65,10 +194,9 @@ export const sesiones = [
 					"En el aula presencial o mediante la videoconferencia, ésta última deberá ser grabada.",
 				],
 				instrucciones: [
-					"1.- En equipo comenten y consensuen el contenido de las infografías que elaboraron previamente de manera individual.",
+					"En equipo comenten el contenido de nube de palabras que elaboraron previamente de manera individual.",
 					"Elaboren una sola infografía y publíquenla en la sección de tareas.",
-					"Recuerda consultar los criterios de evaluación de los organizadores gráficos.",
-					"Asiste a las sesiones, plantea tus dudas y contesta las preguntas dirigidas. ",
+					"Asiste a las sesiones, plantea tus dudas.",
 				],
 			},
 		},
